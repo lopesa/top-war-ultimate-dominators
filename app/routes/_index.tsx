e9709d4b7a2fb7dced4b2ca4f3fea8a3f958c71c
Tony Lopes <tonylopes@tonys-mac-mini.lan>
2023-05-19 16:39:54 -0400

    - non-d3 version is also stalled. Stuck on a html nodes placed on top of a canvas - currently poc of a ping out to a python microservice to create an image visualization  of a simple graph of other data

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,7 @@ import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { Link } from "@remix-run/react";
 // import * as d3 from "d3"
-import { createContext, useEffect, useRef } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 // import ForceGraph from "~/components/d3_ForceGraph"
 import useWindowSize from "~/hooks/useWindowSize";
 import { ElementPositionContextProvider } from "~/contexts/ElementPositionContext";
@@ -15,108 +15,200 @@ import {
   CENTER_NODE_ID,
 } from "~/Constants";
 import GraphEdgesCanvas from "~/components/GraphEdgesCanvas";
+import { link } from "fs";
 
 type D3FormattedData = {
   nodes: { id: string }[];
   links: { source: string; target: string; value: number }[];
 };
+
+type GraphNodeType = {
+  id: string;
+  centerCoordinates?: number[];
+};
 export interface LayoutFormattedData {
-  nodes: { id: string; centerCoordinates?: number[] }[];
-  links: { source: string; target: string; value: number }[];
+  nodes: GraphNodeType[];
+  links: { source: string; target: string; value: number; drawn?: boolean }[];
 }
 
 export const meta: V2_MetaFunction = () => [
   { title: "Top War Ultimate Dominators" },
 ];
 
+// const spreadsheetUrl =
+//   "https://docs.google.com/spreadsheets/d/15PqlUgPb9h5gfiT-3JVImW89cJZwegAbnB5KIe2TGW0/gviz/tq?tq=select%20*&tqx=out:csv";
+
 const spreadsheetUrl =
-  "https://docs.google.com/spreadsheets/d/15PqlUgPb9h5gfiT-3JVImW89cJZwegAbnB5KIe2TGW0/gviz/tq?tq=select%20*&tqx=out:csv";
+  "https://docs.google.com/spreadsheets/d/16eEuq3b7Jlp7kSavEMY4a6voGtaEfTGOhXW-svkxSPw/gviz/tq?tq=select%20*&tqx=out:csv";
+
+const pyUrl = "http://localhost:5000/visualize";
 
 export const loader = async () => {
-  let d3FormattedData: D3FormattedData = {
+  let layoutFormattedData: LayoutFormattedData = {
     nodes: [],
     links: [],
   };
 
   const response = await fetch(spreadsheetUrl);
-  const csvAsText = await response.text();
-  const csvAsArrays: string[][] = csvAsText.split("\n").map((row) => {
-    return row
-      .split(",")
-      .filter((item) => item !== '""')
-      .map((item) => {
-        return item.replace(/\D+/g, "");
-      });
+  const responseBlob = await response.blob();
+
+  const pyResponse = await fetch(pyUrl, {
+    method: "POST",
+    body: responseBlob,
   });
 
-  csvAsArrays.forEach((node) => {
-    d3FormattedData.nodes.push({
-      id: node[0],
-    });
-    for (let i = 1; i < node.length; i++) {
-      d3FormattedData.links.push({
-        source: node[0],
-        target: node[i],
-        value: 1,
-      });
-    }
-  });
-  return json(d3FormattedData);
+  // const csvAsBlob = await pyResponse.blob();
+  const csvAsJson = await pyResponse.json();
+  // const csvAsText = await pyResponse.text();
+
+  // const image = await pyResponse.blob();
+
+  // const csvAsText = await response.text();
+  // const csvAsArrays: string[][] = csvAsText.split("\n").map((row) => {
+  //   return row
+  //     .split(",")
+  //     .filter((item) => item !== '""')
+  //     .map((item) => {
+  //       return item.replace(/\D+/g, "");
+  //     });
+  // });
+
+  // csvAsArrays.forEach((node) => {
+  //   layoutFormattedData.nodes.push({
+  //     id: node[0],
+  //   });
+  //   for (let i = 1; i < node.length; i++) {
+  //     layoutFormattedData.links.push({
+  //       source: node[0],
+  //       target: node[i],
+  //       value: 1,
+  //     });
+  //   }
+  // });
+
+  // const centerNode = layoutFormattedData.nodes.find((node) => {
+  //   return node.id === CENTER_NODE_ID;
+  // });
+
+  // if (!centerNode) {
+  //   return new Error("No center node found");
+  // }
+
+  // return json(layoutFormattedData);
+  return json(csvAsJson);
 };
 
-export default function Index() {
-  const windowSize = useWindowSize();
-  const data = useLoaderData<D3FormattedData>();
-  let layoutData: LayoutFormattedData = {
-    nodes: structuredClone(data.nodes),
-    links: structuredClone(data.links),
-  };
-  const screenCenter = [
-    Math.floor(windowSize.innerWidth / 2),
-    Math.floor(windowSize.innerHeight / 2),
-  ];
-  const centerNodeIndex = layoutData.nodes.findIndex(
-    (node) => node.id === CENTER_NODE_ID
-  );
-  layoutData.nodes[centerNodeIndex].centerCoordinates = screenCenter;
-  let linkedNodes = data.links.reduce((acc, cur) => {
-    if (cur.source === CENTER_NODE_ID) {
+const setDataForNode = (
+  node: GraphNodeType,
+  layoutData: LayoutFormattedData,
+  coordinates?: number[]
+) => {
+  // debugger;
+  if (coordinates) {
+    node.centerCoordinates = coordinates;
+  }
+
+  let linkedNodeIds = layoutData.links.reduce((acc, cur) => {
+    if (cur.source === node.id) {
       acc.push(cur.target);
     }
     return acc;
   }, [] as string[]);
 
-  if (linkedNodes.length !== 4) {
+  layoutData.links.map((link) => {
+    if (link.source === node.id && linkedNodeIds.includes(link.target)) {
+      link.drawn = true;
+    }
+    return link;
+  });
+
+  // debugger;
+
+  const baseCoordinates = node.centerCoordinates;
+
+  if (linkedNodeIds.length === 0 || !baseCoordinates) {
     return;
   }
 
-  layoutData.nodes.find(
-    (node) => node.id === linkedNodes[0]
-  )!.centerCoordinates = [
-    screenCenter[0],
-    screenCenter[1] + NODE_DISTANCE_MULTIPLIER * INITIAL_NODE_DIAMETER,
-  ];
+  let upNode = layoutData.nodes.find((node) => node.id === linkedNodeIds[0]);
+  if (upNode && !upNode?.centerCoordinates) {
+    upNode.centerCoordinates = [
+      baseCoordinates[0],
+      baseCoordinates[1] + NODE_DISTANCE_MULTIPLIER * INITIAL_NODE_DIAMETER,
+    ];
+  }
 
-  layoutData.nodes.find(
-    (node) => node.id === linkedNodes[1]
-  )!.centerCoordinates = [
-    screenCenter[0] + NODE_DISTANCE_MULTIPLIER * INITIAL_NODE_DIAMETER,
-    screenCenter[1],
-  ];
+  let rightNode = layoutData.nodes.find((node) => node.id === linkedNodeIds[1]);
+  if (rightNode && !rightNode?.centerCoordinates) {
+    rightNode.centerCoordinates = [
+      baseCoordinates[0] + NODE_DISTANCE_MULTIPLIER * INITIAL_NODE_DIAMETER,
+      baseCoordinates[1],
+    ];
+  }
 
-  layoutData.nodes.find(
-    (node) => node.id === linkedNodes[2]
-  )!.centerCoordinates = [
-    screenCenter[0],
-    screenCenter[1] - NODE_DISTANCE_MULTIPLIER * INITIAL_NODE_DIAMETER,
-  ];
+  let downNode = layoutData.nodes.find((node) => node.id === linkedNodeIds[2]);
+  if (downNode && !downNode?.centerCoordinates) {
+    downNode.centerCoordinates = [
+      baseCoordinates[0],
+      baseCoordinates[1] - NODE_DISTANCE_MULTIPLIER * INITIAL_NODE_DIAMETER,
+    ];
+  }
 
-  layoutData.nodes.find(
-    (node) => node.id === linkedNodes[3]
-  )!.centerCoordinates = [
-    screenCenter[0] - NODE_DISTANCE_MULTIPLIER * INITIAL_NODE_DIAMETER,
-    screenCenter[1],
+  let leftNode = layoutData.nodes.find((node) => node.id === linkedNodeIds[3]);
+  if (leftNode && !leftNode?.centerCoordinates) {
+    leftNode.centerCoordinates = [
+      baseCoordinates[0] - NODE_DISTANCE_MULTIPLIER * INITIAL_NODE_DIAMETER,
+      baseCoordinates[1],
+    ];
+  }
+
+  // let undrawn = layoutData.links.filter((link) => {
+  //   return !link.drawn;
+  // });
+
+  // debugger;
+
+  // if (undrawn.length > 0) {
+  let linkedNodes = [upNode, rightNode, downNode, leftNode];
+  // debugger;
+  // for (let i = 0; i < linkedNodes.length; i++) {
+  //   if (typeof linkedNodes[i] !== "undefined") {
+  //     setDataForNode(linkedNodes[i] as GraphNodeType, layoutData);
+  //   }
+  // }
+  //   // linkedNodes.forEach((node) => {
+  //   //   debugger;
+  //   //   if (node) {
+  //   //     setDataForNode(node, layoutData);
+  //   //   }
+  //   // });
+  // }
+};
+
+export default function Index() {
+  const windowSize = useWindowSize();
+  const data = useLoaderData<D3FormattedData>();
+  debugger;
+  let layoutData: LayoutFormattedData = {
+    nodes: structuredClone(data.nodes),
+    links: structuredClone(data.links),
+  };
+  // let layoutData: LayoutFormattedData = {
+  //   nodes: structuredClone(data.nodes),
+  //   links: structuredClone(data.links),
+  // };
+  const screenCenter = [
+    Math.floor(windowSize.innerWidth / 2),
+    Math.floor(windowSize.innerHeight / 2),
   ];
+  // const centerNode = layoutData.nodes.find((node) => {
+  //   return node.id === CENTER_NODE_ID;
+  // });
+  // if (!centerNode) {
+  //   return;
+  // }
+  // debugger;
+  // setDataForNode(centerNode, layoutData, screenCenter);
 
   return (
     <ElementPositionContextProvider value={layoutData}>
